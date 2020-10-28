f827d61c85a2d1365c4f85bc564fd1de489ed916
MichaelDPO <michael12112015@gmail.com>
2020-10-28 19:43:24 -0300

    primary version

---
+++ src/conexao/ClassConexao.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conexao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane
+/**
+ *
+ * @author micha
+ */
+public class ClassConexao {
+    
+    public static Connection conectar() {
+        
+        Connection con = null;
+        final String URL = "jdbc:mysql://localhost:3306/fitstab?useTimezone=true&serverTimezone=UTC";;
+        final String USER = "root";
+        final String PASS = "25574531mM*";
+        
+        try {
+            con = DriverManager.getConnection(URL, USER, PASS);
+            return con;
+        } catch (SQLException e) {
+            return null;
+        }
+        
+    }
+    //aa//
+    public static void desconectar(Connection con) {
+        try {
+            con.close();
+        } catch (Exception e) {
